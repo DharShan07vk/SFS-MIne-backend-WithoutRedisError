287ae1f2560e0af64456169d8b61f2fc31032585
DharShan07vk <dharshanvk07@gmail.com>
2025-08-28 23:05:29 +0530

    Add raw body middleware to verify payment webhook

--- src/routes/payments/route.ts
+++ src/routes/payments/route.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { requireAuthToken } from "../../middleware";
+import { requireAuthToken, rawBodyMiddleware } from "../../middleware";
 import { createPayment, getPaymentStatus, verifyPayment, verifyClientPayment } from "./controller";
 
 const paymentRouter = Router();
 
 paymentRouter.post("/create", requireAuthToken("STUDENT"), createPayment);
-paymentRouter.post("/verify", verifyPayment); // Webhook endpoint
+paymentRouter.post("/verify", rawBodyMiddleware,verifyPayment); // Webhook endpoint
 paymentRouter.post("/verify-client", requireAuthToken("STUDENT"), verifyClientPayment); // NEW client endpoint
 paymentRouter.get("/status/:orderId", requireAuthToken("STUDENT"), getPaymentStatus);
 
