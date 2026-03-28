71f81fec3841471471b544018cd8794e181ca5fc
DharShan07vk <dharshanvk07@gmail.com>
2026-03-28 13:42:05 +0530

    email service changed

--- src/routes/email/controller.ts
+++ src/routes/email/controller.ts
@@ -10,9 +10,7 @@ const pass = process.env.APP_PASS;
 
 
 let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
+    service: "gmail",
     auth: {
         user: email,
         pass: pass, 
